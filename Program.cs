0ac09b9648cde7ad1bed651a23c602c2c7cae020
lvlinsane <babygirlimsorrrry@gmail.com>
2026-09-08 17:52:51 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.Write("Введите Ваше ФИО: ");
+        string fio = Console.ReadLine();
+        Console.WriteLine($"Приветствую тебя - {fio}");
+    }
+}
